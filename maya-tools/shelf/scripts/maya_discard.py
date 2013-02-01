253cc75b757c112b0743458dec255e882d5dd343
Nathan Standiford <ns@byu.edu>
2013-02-01 15:03:40 -0700

    Added discard script

---
+++ maya-tools/shelf/scripts/maya_discard.py
@@ -0,0 +1,36 @@
+import maya.cmds as cmds
+import utilities as amu #asset manager utilities
+import brent
+import os
+
+def showWarningDialog(): 
+        return cmds.confirmDialog( title         = 'Discard Confirmation' 
+                                 , message       = 'YOU ARE ABOUT TO IRREVOKABLY DISCARD ALL CHANGES YOU HAVE MADE. '
+                                                   'Please think this through very carefully.\r\n\r\nNow that we have '
+                                                   'gotten that straightened out, are you sure you want to discard '
+                                                   'your changes?'
+                                 , button        = ['Yes', 'No', 'Brent']
+                                 , defaultButton = 'No'
+                                 , cancelButton  = 'No'
+                                 , dismissString = 'No')
+
+def discard():
+        dlgResult = showWarningDialog()
+        if dlgResult == 'Yes':
+                toDiscard = os.path.dirname(filePath) # get discard directory before opening new file
+                cmds.file(force=True, new=True) #open new file
+                amu.Discard(toDiscard) # discard changes
+        else if dlgResult == 'Brent':
+                brent.go()
+        else:
+                cmds.confirmDialog(  title         = 'Discard Cancelled'
+                                   , message       = 'Thank you for being responsible.'
+                                   , button        = ['Ok']
+                                   , defaultButton = 'Ok'
+                                   , cancelButton  = 'Ok'
+                                   , dismissString = 'Ok')
+                                   
+
+def go():
+        discard()
+
