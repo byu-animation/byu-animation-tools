dc3d6fa27055f3822e778082df8160a5fbbffb87
Elizabeth Brayton <ebrayton@gmail.com>
2013-02-04 10:22:13 -0700

    Parsing shot list & setting output dir

--- python2.6libs/weekly_render.py
+++ python2.6libs/weekly_render.py
@@ -2,6 +2,7 @@ import shutil
 import os
 
 LIGHTING_DIR = os.environ['LIGHTING_DIR']
+DAILIES_DIR = os.environ['DAILIES_DIR']
 
 # Validation Functions #
 def _isValidTextFile(p):
@@ -32,18 +33,17 @@ def parseDefinitionFile(filePath):
     shotList = []
     f = open(filePath, 'r')
     for line in f:
-        if not line.startswith('#'):
-            shotInfo = parseShotLine(line)
-            if shotInfo:
-                shotList.append(shotInfo)
-        #TODO
+        shotInfo = parseShotLine(line)
+        if shotInfo:
+            shotList.append(shotInfo)
+    return shotList
 
 # Houdini UI #
 # Return the path to the .txt file to be read.
 def getInputFile():
     inputFile = ''
     while(not _isValidTextFile(inputFile)):
-        inputFile = hou.ui.selectFile(start_directory = None,\
+        inputFile = hou.ui.selectFile(start_directory = hou.expandString(DAILIES_DIR),\
                                       title = "Select Definition (.txt) File",\
                                       collapse_sequences = False,\
                                       pattern = ('*.txt'),\
@@ -51,22 +51,48 @@ def getInputFile():
                                       chooser_mode = hou.fileChooserMode.Read)
         if inputFile == '':
             raise Exception("No input file chosen. Exiting...")
+    
     return hou.expandString(inputFile)
     
-def getOutputDir():
-    hou.ui.displayMessage("Please Select a Render Output Directory.")
-    outputDir= ''
-    outputDir = hou.ui.selectFile(start_directory = None,\
-                                      title = "Select Output Directory for Renders",\
-                                      collapse_sequences = False,\
-                                      file_type = hou.fileType.Directory,\
-                                      multiple_select = False,\
-                                      chooser_mode = hou.fileChooserMode.Read)
-        if outputDir == '':
-            raise Exception("No output directory chosen. Exiting...") #TODO use tmp dir instead
-        return hou.expandString(outputDir)
-                                      
-## Main ##
-inputFile = getInputFile()
-outputDir = getOutputDir()
+def getOutputDir(output = None):
+    if not output:
+        hou.ui.displayMessage("Please Select a Render Output Directory.")
+        outputDir= ''
+        outputDir = hou.ui.selectFile(start_directory = None,\
+                                          title = "Select Output Directory for Renders",\
+                                          collapse_sequences = False,\
+                                          file_type = hou.fileType.Directory,\
+                                          multiple_select = False,\
+                                          chooser_mode = hou.fileChooserMode.Read)
+        if not outputDir:
+            return hou.expandString(output)
 
+        return hou.expandString(outputDir)
+
+    else:
+        return hou.expandString(output)
+
+def weeklyRender(shotList):
+    '''
+    TODO:
+    for each shot
+    a. copy Lighting file (most recent one in the Lighting folder?) into my tmp dir
+    b. open it up (run in houdini?)
+    c. add a prescribed mantra node (settings all happy)
+    \   c.i. create shot folder in output dir?
+    d. set mantra frame range to that described in the file
+    e. create hqueue attached to that mantra
+    f. shoot off hqueue render
+    g. wait for completion?
+    h. delete temp file in tmp dir.
+    i. repeat
+    '''
+
+## Hou Main ##
+inputFile = getInputFile()
+outputDir = getOutputDir(os.path.join(DAILIES_DIR, "renders"))
+shotList = parseDefinitionFile(inputFile)
+try:
+    weeklyRender(shotList)
+except SyntaxError:
+    print ("A syntax error occurred.")
