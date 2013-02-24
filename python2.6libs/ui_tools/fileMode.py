8058f34bb6f2cd9c4377e0805e95aea320ac9608
Ethan D. Estrada <ethan.estrada@gmail.com>
2013-02-23 23:20:33 -0700

    Began moving UI functionality into dedicated Python module
    This makes it possible to run Houdini centric tools in Maya and
    vice-versa. The Houdini tools are almost all ported over. However,
    the Maya tools are not yet abstracted (i.e. all the Maya UI stuff
    is hard coded into the Maya utilities themselves).
    
    This is the start of a general refactoring of code to clean up the
    pipeline tools in general.

---
+++ python2.6libs/ui_tools/fileMode.py
@@ -0,0 +1,13 @@
+'''Acts as an enum of file modes
+
+These should also correspond to POSIX file permission modes.'''
+
+Nothing = 0
+Execute = 1
+Write = 2
+WriteAndExecute = 3
+Read = 4
+ReadAndExecute = 5
+ReadAndWrite = 6
+ReadWriteAndExecute = 7
+
---
+++ python2.6libs/ui_tools/ui.py
@@ -0,0 +1,29 @@
+import os
+import _ui_base
+
+# Make _ui default to _ui_base
+_ui = _ui_base
+
+try:
+    # Check to see what environment we are in and reassign _ui accordingly
+    if os.environ['CURRENT_PROG'] == 'Houdini':
+        import _ui_houdini
+        _ui = _ui_houdini
+    elif os.environ['CURRENT_PROG'] == 'Maya':
+        import _ui_maya
+        _ui = _ui_maya
+except KeyError as ke:
+    # No variable by this name exists
+    pass
+except:
+    # Something else really bad happened
+    pass
+
+# Make local functions aliases of the functions we ought to use
+infoWindow = _ui.infoWindow
+listWindow = _ui.listWindow
+inputWindow = _ui.inputWindow
+passwordWindow = _ui.passwordWindow
+fileChooser = _ui.fileChooser
+shotDialog = _ui.shotDialog
+
