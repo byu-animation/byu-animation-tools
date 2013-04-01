14341560bcddec5152223e1ea5b1ea7aeaa41942
Elizabeth <ebrayton@gmail.com>
2013-04-01 13:50:17 -0600

    Local Main render works.

--- python2.6libs/weekly_render.py
+++ python2.6libs/weekly_render.py
@@ -20,7 +20,7 @@ LIGHTING_SUFFIX = "_lighting_stable"
 LIGHTING_FOLDER_NAME = "lighting/stable"
 HOUDINI_EXTENSION = ".hipnc"
 FRAME_SUFFIX = "_$F3"
-FILE_TYPE = ".tif"
+FILE_TYPE = ".exr"
 
 MANTRA_NODES_PATH = "/out/owned_render_nodes1"
 MANTRA_NAME = "Main"
