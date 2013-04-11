f8cb1e7d97b36ffd66eb5b1247e29bc22d653353
Ethan D. Estrada <ethan.estrada@gmail.com>
2013-04-10 18:08:56 -0600

    Created new utils to force permissions
    hou_asset_mgr.py now uses this. So permissions should be good
    for houdini. Need to implement for Maya (more important really).

--- python2.6libs/hou_asset_mgr.py
+++ python2.6libs/hou_asset_mgr.py
@@ -7,6 +7,7 @@ import os, glob
 import hou
 import subprocess
 from ui_tools import ui, messageSeverity, fileMode
+from miscutil import futil
 
 import utilities as amu #asset manager utilites
 
@@ -94,6 +95,7 @@ def copyToOtlDir(node, filename, newName, newDef):
     newfilepath = os.path.join(OTLDIR, filename)
     oldfilepath = os.path.join(USERDIR, filename)
     node.type().definition().copyToHDAFile(newfilepath, new_name=newName, new_menu_name=newDef)
+    futil.clobberPermissions(newfilepath)
     switchOPLibraries(oldfilepath, newfilepath)
 
 def moveToOtlDir(node, filename):
@@ -110,6 +112,7 @@ def copyToUsrDir(node, filename):
     newfilepath = os.path.join(USERDIR, filename)
     oldfilepath = os.path.join(OTLDIR, filename)
     node.type().definition().copyToHDAFile(newfilepath)
+    futil.clobberPermissions(newfilepath)
     switchOPLibraries(oldfilepath, newfilepath)
 
 def lockOTL(filename):
@@ -446,6 +449,7 @@ def newContainer(hpath):
             amu.createNewAssetFolders(ASSETSDIR, filename)
             templateNode.type().definition().copyToHDAFile(newfilepath, new_name=filename, new_menu_name=name)
             hou.hda.installFile(newfilepath, change_oplibraries_file=True)
+            futil.clobberPermissions(newfilepath)
             newnode = hou.node(hpath).createNode(filename)
         else:
             ui.infoWindow("Asset by that name already exists. Cannot create asset.", wtitle='Asset Name', msev=messageSeverity.Error)
@@ -510,6 +514,7 @@ def rename(node = None):
                             else:
                                 node.type().definition().copyToHDAFile(newfilepath, new_name=newfilename, new_menu_name=name)
                                 hou.hda.installFile(newfilepath, change_oplibraries_file=True)
+                                futil.clobberPermissions(newfilepath)
                                 newnode = hou.node(determineHPATH()).createNode(newfilename)
                                 node.destroy()
                                 hou.hda.uninstallFile(oldlibraryPath, change_oplibraries_file=False)
@@ -732,8 +737,6 @@ def newTexture():
             args = ['txmake','-mode','periodic','-compression','zip']
             args += ['-format','openexr','-half',convertedTexture,finalTexture]
 
-            subprocess.check_call(args)
-
             # Uncomment the following and comment out the previous call if PRMan is not present
             """
             args = 'iconvert -d half ' + convertedTexture + ' ' 
@@ -741,22 +744,30 @@ def newTexture():
 
             subprocess.check_call( args.split() )
             """
-            
-            # Rename texture and move into production pipeline 
-            finalTextureName, ext = os.path.splitext(os.path.basename(finalTexture))
 
-            newTextureName = assetName + '_' + geoName + '_' + shadingPass + ext
- 
-            newfilepath = os.path.join(assetImageDir,newTextureName)
+            try:
+                subprocess.check_call(args)
+            except subprocess.CalledProcessError as e:
+                ui.infoWindow('Failed to convert texture.')
+            else:
+                # Rename texture and move into production pipeline 
+                finalTextureName, ext = os.path.splitext(os.path.basename(finalTexture))
 
-            shutil.copy(finalTexture,newfilepath)
+                newTextureName = assetName + '_' + geoName + '_' + shadingPass + ext
+     
+                newfilepath = os.path.join(assetImageDir,newTextureName)
 
-            # Remove temporary files
-            os.remove(finalTexture)
-            os.remove(convertedTexture)
+                #shutil.copy(finalTexture,newfilepath)
+                futil.copy(finalTexture,newfilepath)
 
-            # Output final success message
-            ui.infoWindow('Your texture was saved to: ' + newfilepath + didgamma)
+                # Output final success message
+                ui.infoWindow('Your texture was saved to: ' + newfilepath + didgamma)
+            finally:
+                # Remove temporary files
+                os.remove(finalTexture)
+                os.remove(convertedTexture)
+
+
 
 def getNodeInfo(node):
     if node != None and isDigitalAsset(node):
